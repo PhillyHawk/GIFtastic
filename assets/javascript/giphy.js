67046dbcc77e65b666252da4e0f8a23c4f5ec252
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-10 19:27:57 -0400

    adding button triggered code

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,22 +2,42 @@ var monsters = [
   "Dracula", "Frankenstein", "Mummy", "Zombies", "Godzilla", "Clowns", "Elvira", "The Munsters", "Cousin Itt", "Gremlins", "Killer Tomatoes"
 ]
 
-function renderButtons(){
+function renderButtons() {
   $("buttons-view").empty();
 
-  for (var i =0; i < monsters.length; i++){
+  for (var i = 0; i < monsters.length; i++) {
     var a = $("<button>");
     a.addClass("monster");
-    a.attr("data-name", monsters[i]);
+    a.attr("data-monster", monsters[i]);
     a.text(monsters[i]);
     $("#buttons-view").append(a);
   }
 }
-$("#add-monster").on("click", function(event){
+$("#add-monster").on("click", function (event) {
   event.preventDefault();
   var monster = $("#monster-input").val().trim();
   monsters.push(monster);
   renderButtons();
 });
 renderButtons();
-console.log("mars")
+$("button").on("click", function () {
+  var monster = $(this).attr("data-monster");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    monster + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+})
+  .then(function (response) {
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+      var gifDiv = $("<div");
+      var monsterImage = $("<img>");
+      monsterImage.attr("src", results[i].images.fixed_height.url);
+      $("#gifs-appear-here").prepend(gifDiv);
+
+    }
+  });
