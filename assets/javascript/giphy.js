9941f6722ef0f4f7c5116bae11c0cd5120bc0838
Hawk Shannon <hawk.dillinger@gmail.com>
2019-07-11 16:26:56 -0400

    gifs appear when button is pressed

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,28 +1,46 @@
+$(document).ready(function(){
+//array of monsters
 var monsters = [
   "Dracula", "Frankenstein", "Mummy", "Zombies", "Godzilla", "Clowns", "Elvira", "The Munsters", "Cousin Itt", "Gremlins", "Killer Tomatoes"
 ]
-
+//function for displaying monster data
 function renderButtons() {
-  $("buttons-view").empty();
 
+  //deleting the monster buttons prior to adding  new
+  $("#buttons-view").empty();
+
+  //looping through th earray of monsters
   for (var i = 0; i < monsters.length; i++) {
+
+    //dynamically generating buttons for each monster in the array
     var a = $("<button>");
+    //adding a class
     a.addClass("monster");
-    a.attr("data-monster", monsters[i]);
+    //adding data-attribute with a value of the monster at index i
+    a.attr("data-name", monsters[i]);
+    //providing the button's text with a value of the movie at index i
     a.text(monsters[i]);
+    //adding the button to  the HTML
     $("#buttons-view").append(a);
   }
 }
+//This function handles events where one button is clicked
 $("#add-monster").on("click", function (event) {
+  //prevents the form from trying to submit itself
   event.preventDefault();
+
+  //this line will grab the text from thr inpuut box
   var monster = $("#monster-input").val().trim();
+ //the monster from the textbox is  then added to the array
   monsters.push(monster);
   renderButtons();
-  return;
 });
 renderButtons();
-$("button-view").on("click", function () {
-  var monster = $(this).attr("data-monster");
+
+$(document).on("click", ".monster", function() {
+// $(".monster").on("click", function () {
+  var monster = $(this).attr("data-name");
+  console.log(monster);
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     monster + "&api_key=UK3iOXI0JIkyMjSg5vYkp1TrsA9astgo&limit=10";
 
@@ -30,12 +48,12 @@ $("button-view").on("click", function () {
     url: queryURL,
     method: "GET"
   })
-})
   .then(function (response) {
     var results = response.data;
 
     for (var i = 0; i < results.length; i++) {
-      var gifDiv = $("<div");
+      var gifDiv = $("<div>");
+      gifDiv.addClass('monsterDiv')
       var rating = results[i].rating;
       var p = $("<p>").text("Rating: " + rating);
       var monsterImage = $("<img>");
@@ -46,4 +64,6 @@ $("button-view").on("click", function () {
       $("#gifs-appear-here").prepend(gifDiv);
       console.log(response)
     }
+  })
   });
+});
